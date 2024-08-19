22c99fdde7ae6f7fd11a559ccc6187f5b203b6c8
PerveevEvgen <113028833+PerveevEvgen@users.noreply.github.com>
2024-08-19 09:16:00 +0300

    bug reports added

---
+++ Issue Tracker. QATestLab_files/javascript_translations.php
@@ -0,0 +1,4 @@
+var translations = new Array();
+translations['time_tracking_stopwatch_start'] = 'Запустить';
+translations['time_tracking_stopwatch_stop'] = 'Остановить';
+translations['loading'] = 'Загрузка...';
